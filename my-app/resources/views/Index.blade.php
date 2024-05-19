a3a171c732da854bfa6f46aa419d6abd8ca6cbf5
MarkAhronTaglucop <mtaglucop2@gmail.com>
2024-05-19 08:11:50 +0800

    new changes

---
+++ my-app/resources/views/Index.blade.php
@@ -0,0 +1,24 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>IT 106</title>
+    <link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
+
+</head>
+<body>
+
+    <a href="logout.php">logout</a>
+    <h1>This is the index page</h1>
+    <br>
+
+
+
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>  
+
+</body>
+</html>
+
